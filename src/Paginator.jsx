7dd560c0f4f33dd923f8d743cc9ee85b9ef79dfa
fabio <blackjackdevel@gmail.com>
2016-02-27 20:35:44 -0300

    Added package.json , changed folder structure

--- src/Paginator.jsx
+++ src/Paginator.jsx
@@ -12,9 +12,13 @@ export const Paginator = React.createClass({
         let current = this.props.current;
         let total = this.props.total;
         let max = this.props.max;
+
         let start = Math.max(current - Math.round(max/2) ,1);
 
         let end = Math.min(start+max,total);
+        if(end - start < max) {
+            start = Math.max(1,end - max);
+        }
         let hasLess = null;
         let previous = null;
         if(start != 1){
